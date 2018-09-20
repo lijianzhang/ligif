4ac1f026bf8d521020837a6f3c10f7712ed6d4fd
lijianzhang <coder_jz@163.com>
2018-09-21 00:56:45 +0800

    :recycle: 迭代

--- src/work.ts
+++ src/work.ts
@@ -2,13 +2,19 @@
  * @Author: lijianzhang
  * @Date: 2018-09-21 00:28:46
  * @Last Modified by: lijianzhang
- * @Last Modified time: 2018-09-21 00:48:48
+ * @Last Modified time: 2018-09-21 00:53:55
  */
 
-export default function work(fn: Function) {
-    var str = "onmessage=function(e){postMessage(" + fn + "(e.data))}";
-    const blog = new Blob([str], { type: 'application/javascript' });
-    const work = new Worker(URL.createObjectURL(blog));
+export default function work(fn: Function | Blob) {
+    let blob: Blob;
+    if (fn instanceof Blob) {
+        blob = fn;
+    } else {
+        var str = "onmessage=function(e){postMessage(" + fn + "(e.data))}";
+        blob = new Blob([str], { type: 'application/javascript' });
+    }
+
+    const work = new Worker(URL.createObjectURL(blob));
 
     return (message: any) => {
         work.postMessage(message);
